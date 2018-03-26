b0368265da54a0a029a9430e0a4d01cdcb5bd9ce
Mark Muskardin <mark.muskardin@gmail.com>
2018-03-26 12:59:30 -0700

    updated BEGIN folder for lecture 2

--- lectures/2-connect-ui-to-smart-contract/BEGIN/src/core/reducers/reducer-asset.js
+++ lectures/2-connect-ui-to-smart-contract/BEGIN/src/core/reducers/reducer-asset.js
@@ -1,7 +1,12 @@
 import constants from 'core/types'
 
 const initialState = {
-  stagedAsset: null
+  stagedAsset: null,
+  assetHash: '',
+  alreadyExists: false,
+  error: '',
+  transaction: null,
+  success: false
 }
 
 export function assetReducer(state = initialState, action) {
@@ -10,6 +15,24 @@ export function assetReducer(state = initialState, action) {
   case constants.ADD_ASSET:
     return Object.assign({}, state, {
       stagedAsset: action.asset[0]
+    })
+
+  case constants.CREATE_ASSET_HASH:
+    return Object.assign({}, state, {
+      assetHash: action.hash,
+      success: action.success,
+      transaction: action.transaction
+    })
+
+  case constants.CHECK_ASSET:
+    return Object.assign({}, state, {
+      assetHash: action.assetHash,
+      alreadyExists: action.alreadyExists
+    })
+
+  case constants.ASSET_ERROR:
+    return Object.assign({}, state, {
+      error: action.error
     })
 
   case constants.CLEAR_ASSETS:
